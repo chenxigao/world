864f4325907e3c391dbdc377e814fc454bdeb012
chenxigao <819603673@qq.com>
2018-05-01 02:49:12 +0000

    first

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+   <footer class="footer">
+       <small class="slogon">
+           <img class="brand-icon" src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+          <a href="https://laravel-china.org/courses">优帆远扬|生命不息，创造不止</a>
+       </small>
+       <nav>
+         <ul>
+            <li><a href="{{ route('about') }}">关于</a></li>
+         </ul>
+       </nav>
+   </footer>
+</div>
