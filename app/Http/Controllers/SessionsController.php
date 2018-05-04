70ade13b7e5970209f5f411bbf4c3c20ffcac1cc
chenxigao <819603673@qq.com>
2018-05-04 06:04:03 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -10,6 +10,8 @@ class SessionsController extends Controller
     public function create(){
         return view('sessions.create');
     }
+
+
     public function store(Request $request){
 
         $credentials = $this->validate($request,[
@@ -17,10 +19,17 @@ class SessionsController extends Controller
             'password'=>'required'
         ]);
 
-        if(Auth::attempt($credentials,$request->has('remember'))){
+        if (Auth::attempt($credentials, $request->has('remember'))){
+            if(Auth::user()->activated){
+
             //登录成功后的相关操作
             session()->flash('success','欢迎回来');
             return redirect()->intended(route('users.show',[Auth::user()]));
+        } else {
+                Auth::logout();
+                session()->flash('warning','您的账号未激活，请检查邮箱中的注册邮件进行激活');
+                return redirect('/');
+            }
         } else {
             //登录失败后的操作
             session()->flash('danger','很抱歉，您的邮箱和密码不符');
@@ -28,6 +37,7 @@ class SessionsController extends Controller
 
         }
     }
+
 
     public function destroy()
     {
